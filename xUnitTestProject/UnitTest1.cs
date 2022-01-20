9f20720782fb5ba7fa21f854e5617d2d96e99c4f
Michiel Doeven <m.doeven@sharevalue.nl>
2022-01-20 14:20:59 +0100

    Geëxtrapoleerde const

--- xUnitTestProject/UnitTest1.cs
+++ xUnitTestProject/UnitTest1.cs
@@ -3,6 +3,9 @@ namespace xUnitTestProject
 {
     public class UnitTest1
     {
+        public const string FullProductName = $"Envrionment: {Platform}";
+        public const string Platform = "test";
+
         [Fact]
         public void Test1()
         {
