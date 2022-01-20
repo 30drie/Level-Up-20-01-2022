ff6a5768c145e9de7a1c85beb90e54d2bf596a56
Michiel Doeven <m.doeven@sharevalue.nl>
2022-01-20 12:07:34 +0100

    Add project files.

---
+++ InterpolatedString/Logger.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleApp1;
+public enum LogLevel
+{
+    Off,
+    Critical,
+    Error,
+    Warning,
+    Information,
+    Trace
+}
+
+public class Logger
+{
+    public LogLevel EnabledLevel { get; init; } = LogLevel.Error;
+
+    public void LogMessage(LogLevel level, string msg)
+    {
+        if (EnabledLevel < level) return;
+        Console.WriteLine(msg);
+    }
+
+    public void LogMessage(LogLevel level, LogInterpolatedStringHandler builder)
+    {
+        if (EnabledLevel < level) return;
+        Console.WriteLine(builder.GetFormattedText());
+    }
+}
+
+[InterpolatedStringHandler]
+public ref struct LogInterpolatedStringHandler
+{
+    // Storage for the built-up string
+    StringBuilder builder;
+
+    public LogInterpolatedStringHandler(int literalLength, int formattedCount)
+    {
+        builder = new StringBuilder(literalLength);
+        Console.WriteLine($"\tliteral length: {literalLength}, formattedCount: {formattedCount}");
+    }
+
+    public void AppendLiteral(string s)
+    {
+        Console.WriteLine($"\tAppendLiteral called: {{{s}}}");
+
+        builder.Append(s);
+        Console.WriteLine($"\tAppended the literal string");
+    }
+
+    public void AppendFormatted<T>(T t)
+    {
+        Console.WriteLine($"\tAppendFormatted called: {{{t}}} is of type {typeof(T)}");
+
+        builder.Append(t?.ToString());
+        Console.WriteLine($"\tAppended the formatted object");
+    }
+
+    internal string GetFormattedText() => builder.ToString();
+}
---
+++ InterpolatedString/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var logger = new Logger() { EnabledLevel = LogLevel.Warning };
+var time = DateTime.Now;
+
+logger.LogMessage(LogLevel.Error, $"Error Level. CurrentTime: {time}. This is an error. It will be printed.");
+logger.LogMessage(LogLevel.Trace, $"Trace Level. CurrentTime: {time}. This won't be printed.");
+logger.LogMessage(LogLevel.Warning, "Warning Level. This warning is a string, not an interpolated string expression.");
